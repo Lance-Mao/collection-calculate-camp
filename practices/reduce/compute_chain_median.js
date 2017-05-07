61b791b384913353d5e47615fe08ef9ad152ef89
M977986310 <977986310@qq.com>
2017-05-07 20:52:55 +0800

    5.7

--- practices/reduce/compute_chain_median.js
+++ practices/reduce/compute_chain_median.js
@@ -2,6 +2,21 @@
 
 function compute_chain_median(collection) {
   //在这里写入代码
+
+  var num = 0;
+  collection = collection.split("->");
+  collection.sort(function (a, b) {
+    return a - b > 0 ? 1 : -1
+  });
+  if (collection.length % 2 === 0) {
+    num = (parseInt(collection[collection.length / 2 - 1]) + parseInt(collection[(collection.length / 2 )])) / 2;
+  } else {
+    num = parseInt(collection[((collection.length + 1) / 2)]);
+  }
+  return num;
+  /*
+   return result[result.length-1) / 2];
+   */
 }
 
 module.exports = compute_chain_median;
--- specs/reduce/compute_chain_median_spec.js
+++ specs/reduce/compute_chain_median_spec.js
@@ -6,7 +6,7 @@ describe('compute_chain_median', function () {
 
   var chain = '1->4->6->2->3->10->9->8->11->20->19->30';
 
-  it('计算给定链表的中位数', function () {
+  iit('计算给定链表的中位数', function () {
 
     var result = compute_chain_median(chain);
     expect(result).toEqual(8.5);
