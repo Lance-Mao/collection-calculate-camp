ed7fcac13d118e7cc842ace2734aa9389ffe0205
M977986310 <977986310@qq.com>
2017-05-10 20:38:40 +0800

    practice_3.js

--- practices/section_2/practice_3/practice.js
+++ practices/section_2/practice_3/practice.js
@@ -1,31 +1,23 @@
 function count_same_elements(collection) {
-  //在这里写入代码
-  var result = [];
-  var index = 0;
-  var sum = 0;
-  while (true) {
-    var count = 0;
-    index = sum;
-    for (var i in collection) {
-      if (collection[index] === collection[i]) {
-        count++;
+  collection.forEach((element, index) => {
+    if (element.length > 1) {
+      let temporaryArray = element.split("");
+      let number = parseInt(element.replace(/[^0-9]/ig, ""));
+      collection.splice(index, 1);
+      for (let a = 0; a < number; a++) {
+        collection.splice(index, 0, temporaryArray[0])
       }
-    }
-    sum += count;
-    if (collection[index].indexOf("-")!== -1) {
-      result.push({name:collection[index].indexOf("-")[0], summary: collection[index].indexOf("-")[1]});
-    }
-    if (collection[index].indexOf(":") !== -1) {
-      result.push({name: collection[index].indexOf(":")[0], summary: collection[index].indexOf(":")[1]});
-    }
-    result.push({name: collection[index], summary: count});
 
-    if (sum >= collection.length) {
-      break;
     }
-  }
+  });
+  let result = [];
 
-  return result;
+  collection.forEach(item => {
+    if (!result.find(element => element.key === item))
+      result.push({key: item, count: collection.filter(element => element === item).length});
+  });
+
+  return result
 }
 
 module.exports = count_same_elements;
