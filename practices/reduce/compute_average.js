4d2aa122ef9a7e468e5e6cff225572e24226e92c
admin-m <977986310@qq.com>
2017-07-28 10:17:20 +0800

    代码优化

--- practices/reduce/compute_average.js
+++ practices/reduce/compute_average.js
@@ -1,12 +1,8 @@
 'use strict';
 
 function compute_average(collection) {
-  //在这里写入代码
-  var sum= 0;
-  for (var i in collection) {
-    sum += collection[i];
-  }
-  return sum / collection.length;
+
+  return collection.reduce((a, b) => a + b)/collection.length;
 }
 
 module.exports = compute_average;
