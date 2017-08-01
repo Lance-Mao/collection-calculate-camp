b8e759c1634f4036357d357ed7c799e4c016bd08
admin-m <977986310@qq.com>
2017-08-01 18:41:02 +0800

    优化代码

--- practices/section_3/practice_1/practice.js
+++ practices/section_3/practice_1/practice.js
@@ -1,12 +1,8 @@
 function create_updated_collection(collection_a, object_b) {
-  //在这里写入代码
-  for (var i in collection_a) {
-    for (var j in object_b.value) {
-      if (collection_a[i].key === object_b.value[j]) {
-        collection_a[i].count = collection_a[i].count -= 1;
-      }
-    }
-  }
+  collection_a.map(item =>{
+  if (object_b.value.includes(item.key)) {
+    item.count--
+  }})
 
   return collection_a;
 }
--- practices/section_3/practice_2/practice.js
+++ practices/section_3/practice_2/practice.js
@@ -1,20 +1,11 @@
 function create_updated_collection(collection_a, object_b) {
-  //在这里写入代码
-  for (var i in collection_a) {
-    for (var j in object_b.value) {
-      if (collection_a[i].key === object_b.value[j]) {
-        if (collection_a[i].count % 3 === 0) {
-          collection_a[i].count = collection_a[i].count -= collection_a[i].count / 3;
-        } else if (collection_a[i].count % 3 === 1) {
-          collection_a[i].count = collection_a[i].count -= (collection_a[i].count - 1 ) / 3;
-        } else if (collection_a[i].count % 3 === 2) {
-          collection_a[i].count = collection_a[i].count -= (collection_a[i].count-2) / 3;
-        }
-      }
-    }
-  }
+  let objectBValues = object_b.value;
+
+  collection_a.map(element => {
+    if (objectBValues.includes(element.key))
+      if(element.count >= 3) element.count -= parseInt(element.count / 3);
+  })
 
   return collection_a;
 }
-
 module.exports = create_updated_collection;
