405601fdce841e67eb257e0762f223366af207ca
admin-m <977986310@qq.com>
2017-11-02 13:49:28 +0800

    修改代码

--- practices/section_2/practice_2/practice.js
+++ practices/section_2/practice_2/practice.js
@@ -1,22 +1,25 @@
 function count_same_elements(collection) {
-  var result = [];
 
-  collection.forEach((element, index) => {
-    var item = element.split("-");
-    if (item.length > 1) {
-      collection.splice(index, 1);
-      for (var i = 0; i < item[1]; i++) {
-        collection.push(item[0]);
-      }
-    }
-  });
 
-  collection.forEach(item => {
-    if (!result.find(element => element.key === item))
-      result.push({key: item, count: collection.filter(element => element === item).length});
-  });
-
-  return result;
+  // let array = [];
+  //
+  // let times = 1;
+  // for (let i = 0; i < collection.length; i++) {
+  //   let element = collection[i].split("-");
+  //   if (collection[i] === collection[i + 1]) {
+  //     times++;
+  //   } else {
+  //     if (element.length <= 1) {
+  //       array.push({key: element[0], count: times});
+  //     } else {
+  //       array.push({key: element[0], count: Number(element[1])});
+  //     }
+  //
+  //     times = 1;
+  //   }
+  // }
+  //
+  // return array;
 }
 
 module.exports = count_same_elements;
