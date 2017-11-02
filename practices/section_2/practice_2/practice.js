2766bb088f6e495be34d1e6298469fca46f80400
admin-m <977986310@qq.com>
2017-11-02 17:00:40 +0800

    添加代码

--- practices/section_2/practice_2/practice.js
+++ practices/section_2/practice_2/practice.js
@@ -1,11 +1,11 @@
 function count_same_elements(collection) {
-  var result = [];
+  const result = [];
 
   collection.forEach((element, index) => {
-    var item = element.split("-");
+    const item = element.split("-");
     if (item.length > 1) {
       collection.splice(index, 1);
-      for (var i = 0; i < item[1]; i++) {
+      for (let i = 0; i < item[1]; i++) {
         collection.push(item[0]);
       }
     }
@@ -17,6 +17,26 @@ function count_same_elements(collection) {
   });
 
   return result;
+
+  // let array = [];
+  //
+  // let times = 1;
+  // for (let i = 0; i < collection.length; i++) {
+  //   let element = collection[i].split("-");
+  //   if (collection[i] === collection[i + 1]) {
+  //     times++;
+  //   } else {
+  //     if (element.length <= 1) {
+  //       array.push({key: element[0], count: times});
+  //     } else {
+  //       array.push({key: element[0], count: Number(element[1])});
+  //     }
+  //
+  //     times = 1;
+  //   }
+  // }
+  //
+  // return array;
 }
 
 module.exports = count_same_elements;
